c7e10ba66f0107cc56ed32f3b28f88a45efc62a7
Mahan <mahan.rahmati@protonmail.com>
2025-01-02 23:54:56 -0500

    feat: add bounded box

--- lib/infinity.dart
+++ lib/infinity.dart
@@ -7,6 +7,7 @@ export 'src/constants/colors.dart';
 export 'src/constants/dimens.dart';
 export 'src/constants/theme.dart';
 export 'src/constants/typography.dart';
+export 'src/widgets/bounded_box.dart';
 export 'src/widgets/boxed_list.dart';
 export 'src/widgets/card.dart';
 export 'src/widgets/divider.dart';
---
+++ lib/src/widgets/bounded_box.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/widgets.dart';
+
+import '../constants/dimens.dart';
+
+/// A container widget that centers and constrains its child's width and
+/// follows Infinity's design system.
+class BoundedBox extends StatelessWidget {
+  /// Creates a bounded box that centers and constrains its child.
+  ///
+  /// [child] the widget to display inside the bounded box with constrained
+  /// width.
+  const BoundedBox({
+    super.key,
+    this.child,
+  });
+
+  /// The widget to display inside the bounded box.
+  final Widget? child;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: InfinityDimens.iphone16ProMaxLandscapeWidth,
+        ),
+        child: child,
+      ),
+    );
+  }
+}
