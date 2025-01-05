3531f811d106038beceec658513e38b158b62f3e
Mahan <mahan.rahmati@protonmail.com>
2025-01-04 19:44:57 -0500

    feat: add buttons to example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,12 +27,77 @@ class ExampleHome extends StatelessWidget {
       body: IBoundedBox(
         child: Column(
           children: <Widget>[
-            ICard(
-              child: Text('Test Card Widget'),
-            ),
+            Buttons(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Buttons extends StatelessWidget {
+  const Buttons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IBoxedList(
+      header: const Text('Buttons'),
+      children: [
+        Wrap(
+          alignment: WrapAlignment.center,
+          children: [
+            IButton.text(
+              onPressed: () {},
+              text: 'Primary',
+            ),
+            IButton.text(
+              text: 'Disabled',
+            ),
+            IButton.text(
+              statusType: StatusType.error,
+              onPressed: () {},
+              text: 'Error',
+            ),
+            IButton.text(
+              statusType: StatusType.warning,
+              onPressed: () {},
+              text: 'Warning',
+            ),
+            IButton.text(
+              statusType: StatusType.success,
+              onPressed: () {},
+              text: 'Success',
+            ),
+          ],
+        ),
+        Wrap(
+          alignment: WrapAlignment.center,
+          children: [
+            IButton.icon(
+              onPressed: () {},
+              icon: MingCuteIcons.mgc_star_line,
+            ),
+            IButton.icon(
+              icon: MingCuteIcons.mgc_star_line,
+            ),
+            IButton.icon(
+              statusType: StatusType.error,
+              onPressed: () {},
+              icon: MingCuteIcons.mgc_delete_line,
+            ),
+            IButton.icon(
+              statusType: StatusType.warning,
+              onPressed: () {},
+              icon: MingCuteIcons.mgc_warning_line,
+            ),
+            IButton.icon(
+              statusType: StatusType.success,
+              onPressed: () {},
+              icon: MingCuteIcons.mgc_check_line,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
