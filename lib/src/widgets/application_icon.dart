63aab073b0a7fe944073781ba160693c3f30cc40
Mahan <mahan.rahmati@protonmail.com>
2025-01-07 20:07:41 -0500

    feat: update application icon

--- lib/src/widgets/application_icon.dart
+++ lib/src/widgets/application_icon.dart
@@ -5,6 +5,19 @@ import 'squircle.dart';
 
 /// An application icon widget that follows Infinity's design system.
 class IApplicationIcon extends StatelessWidget {
+  /// Creates an Infinity application icon.
+  ///
+  /// [child] the widget to display inside the icon.
+  /// [size] optional size constraint for both width and height.
+  IApplicationIcon(
+    final Widget child, {
+    super.key,
+    this.size,
+  }) : child = SizedBox.square(
+          dimension: size,
+          child: child,
+        );
+
   /// Creates an Infinity application icon from an asset image.
   ///
   /// [name] the asset image path to load.
